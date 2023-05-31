f1c2bb3a7c3df5967ea0211d3ab419d5828fac14
Ajelandro19 <almon190699@gmail.com>
2023-05-31 13:54:13 -0600

    Begin challenge

---
+++ script.js
@@ -0,0 +1,7 @@
+var button_encriptar = document.querySelector(
+  "input[type='button'][value='Encriptar']"
+);
+
+button_encriptar.addEventListener("click", function () {
+  console.log("Encriptando...");
+});
